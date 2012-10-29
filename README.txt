a55ba1d2608e9773094b2c1f7708aed0050eb1ae
Lukas Graf <lukas.graf@4teamwork.ch>
2012-10-29 16:16:44 +0100

    Initial commit - empty package.

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This product provides some commonly used utility functions and scripts
+for OpenGever maintenance, running `bin/instance run` scripts or tasks
+done in debug mode.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+setup(name='opengever.maintenance',
+      version=version,
+      description="Commonly used utilities and scripts for OG maintenance.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Lukas Graf',
+      author_email='lukas.graf@4teamwork.ch',
+      url='http://www.4teamwork.ch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opengever'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
