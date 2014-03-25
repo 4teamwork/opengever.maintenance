14e84752bb69804f57a046d5807f0846a5d5a92d
Lukas Knoepfel <shylux@gmail.com>
2014-03-25 16:41:18 +0100

    Added script to check and fix mimetypes.

--- opengever/maintenance/scripts/fix_mime_types.py
+++ opengever/maintenance/scripts/fix_mime_types.py
@@ -5,18 +5,24 @@ from Products.CMFCore.utils import getToolByName
 import transaction
 
 
-def list_objects_with_specific_mime_types(portal, specific_type):
-    """ Lists all plone objects containing a file with the given mimetype.
+def fix_wrong_mime_types(portal, wrong_mimetype, fix):
+    """ Fixes mimetypes of files in plone objects.
 
-    Also mentiones the mimetype according to the plone registry.
+    'wrong_mimetype' is the mimetype the file currenty has. It will be
+    replaced by the value looked up in the plone/python mimetype registry.
     """
-    print "List of objects with wrong mimetype '%s':" % specific_type
-    for obj in get_objects_by_mimetype(portal, specific_type):
-        lookuped_mimetype = lookup_mimetype(portal, obj.file.filename)
-        print "Looked up mimetype '%s' for %s (correct: %s)" % \
-            (obj.file.contentType,
-             '/'.join(obj.getPhysicalPath()),
-             lookuped_mimetype)
+    for obj in get_objects_by_mimetype(portal, wrong_mimetype):
+        registry_type = lookup_mimetype(portal, obj.file.filename).mimetypes[0]
+
+        if registry_type and registry_type != obj.file.contentType:
+            if not fix:
+                print "%s: %s" % (
+                    registry_type.ljust(30),
+                    obj.file.filename)
+            else:
+                obj.file.contentType = registry_type
+
+    transaction.commit()
 
 
 def get_objects_by_mimetype(portal, specific_type):
@@ -38,38 +44,19 @@ def get_objects_by_mimetype(portal, specific_type):
     return result
 
 
-def fix_wrong_mime_types(portal, wrong_type, expected_type=None):
-    """ Fixes mimetypes of files in plone objects.
-
-    'wrong_type' is the mimetype the file currenty has. It will be
-    replaced by 'expected_type'. If 'expected_type' is not specified,
-    the correct value according to the plone registry will be used.
-    """
-    for obj in get_objects_by_mimetype(portal, wrong_type):
-        if not expected_type:
-            expected_type = lookup_mimetype(portal, obj.file.filename).mimetypes[0]
-        old_type = obj.file.contentType
-        obj.file.contentType = expected_type
-        print "Fixed mimetype on %s (%s). '%s' is now '%s'" % \
-            ('/'.join(obj.getPhysicalPath()),
-             obj.file.filename,
-             old_type,
-             obj.file.contentType)
-    transaction.commit()
-
-
 def lookup_mimetype(portal, filename):
     """ Looks up mimetype to a given filename.
 
     It will check the plone mimetype registry and
     fall back to the python mimetype module if needed.
+    Returns None if lookup fails.
     """
     registry = getToolByName(portal, 'mimetypes_registry')
     type_from_registry = registry.lookupExtension(filename)
     if not type_from_registry:
         type_from_registry = registry.globFilename(filename)
     if not type_from_registry:
-        raise Exception("Cannot parse mimetype for %s." % filename)
+        return None
     return type_from_registry
 
 
@@ -77,9 +64,10 @@ def main():
     app = setup_app()
     parser = setup_option_parser()
     parser.add_option("-f", "--fix", action="store_true", dest="fix",
-        help="Flags whether the script should fix or just display the wrong mimetypes.")
+                      help="""Flags whether the script should fix the
+                      "types or just display them.""")
     parser.add_option("-t", "--mimetype", dest="specific_type",
-        help="Search for the given MIMETYPE.")
+                      help="Search for the given MIMETYPE.")
     (options, args) = parser.parse_args()
     plone = setup_plone(app, options)
 
@@ -87,10 +75,7 @@ def main():
         print "No MIMETYPE given."
         return
 
-    if not options.fix:
-        list_objects_with_specific_mime_types(plone, options.specific_type)
-    else:
-        fix_wrong_mime_types(plone, options.specific_type)
+    fix_wrong_mime_types(plone, options.specific_type, options.fix)
 
 if __name__ == '__main__':
     main()
