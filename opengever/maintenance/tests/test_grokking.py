ef0cbc6e47b8a401ec5f532a31ce090f07ed1062
Lukas Graf <lukas.graf@4teamwork.ch>
2015-06-11 12:06:56 +0200

    Add testing setup to test against og.core 2.7 - current.

---
+++ opengever/maintenance/tests/test_grokking.py
@@ -0,0 +1,18 @@
+from opengever.maintenance.testing import OG_MAINTENANCE_INTEGRATION
+import unittest2
+
+
+class TestZCML(unittest2.TestCase):
+
+    layer = OG_MAINTENANCE_INTEGRATION
+
+    def test_zcml_loading(self):
+        """Load the ZCML for the opengever.maintenance package in order to
+        trigger any import errors that would prevent the instance from booting.
+        """
+
+        self.layer['load_zcml_string']("""
+            <configure>
+                <include package="opengever.maintenance" />
+            </configure>
+        """)
