5cae43cd45b4c642270a2cb2362ea25a27d73817
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-13 09:32:08 +0200

    Remove opengever.pdfconverter

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(name='opengever.maintenance',
       analyze-logs = opengever.maintenance.analyze_logs:main
 
       [zopectl.command]
-      convert_missing_pdfs = opengever.maintenance.commands:convert_missing_pdfs_cmd
       get_profile_version = opengever.maintenance.commands:get_profile_version_cmd
       set_profile_version = opengever.maintenance.commands:set_profile_version_cmd
       """,
