832094ae2f298938b5ecf6f0f5ebb0fbb95a9540
Joni Orponen <j.orponen@4teamwork.ch>
2018-08-20 15:12:46 +0200

    Also account for ungettable objects on the missing intid script.

--- opengever/maintenance/scripts/find_objects_with_missing_intids.py
+++ opengever/maintenance/scripts/find_objects_with_missing_intids.py
@@ -9,17 +9,25 @@ import argparse
 def main(portal):
     intid_util = api.portal.getUtility(IIntIds)
     for brain in portal.portal_catalog.unrestrictedSearchResults():
-        obj = brain.getObject()
+        try:
+            obj = brain.getObject()
+        except KeyError:
+            path = brain.getPath()
+            portal_type = brain.portal_type
+            created = str(brain.created)
+            logger.error('Cannot get object of brain %s of portal type %s created on %s', path, portal_type, created)
+            continue
         try:
             intid_util.getId(obj)
         except KeyError:
             path = '/'.join(obj.getPhysicalPath())
             portal_type = obj.portal_type
             created = str(obj.created())
-            logger.error('IntId missing on %s of portal type %s created on %s', path, portal_type, created)
+            logger.error('IntId missing on object %s of portal type %s created on %s', path, portal_type, created)
 
 
 if __name__ == '__main__':
+    logger = getLogger()
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', dest='site_root', default=None, help='Absolute path to the Plone site')
     options = parser.parse_args(sys.argv[3:])
