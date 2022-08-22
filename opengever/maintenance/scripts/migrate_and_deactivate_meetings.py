e8aa14631e9df95a5fa35ca7afb85e592e251c2b
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-08-22 15:08:41 +0200

    Copy state of meeting dossier to newly created normal dossier.

--- opengever/maintenance/scripts/migrate_and_deactivate_meetings.py
+++ opengever/maintenance/scripts/migrate_and_deactivate_meetings.py
@@ -6,6 +6,7 @@ from Acquisition import aq_parent
 from collective.taskqueue.interfaces import ITaskQueue
 from collective.taskqueue.interfaces import ITaskQueueLayer
 from collective.taskqueue.taskqueue import LocalVolatileTaskQueue
+from opengever.base.interfaces import IOpengeverBaseLayer
 from opengever.base.model.favorite import Favorite
 from opengever.base.oguid import Oguid
 from opengever.base.transport import BASEDATA_KEY
@@ -13,6 +14,8 @@ from opengever.base.transport import DexterityObjectCreator
 from opengever.base.transport import DexterityObjectDataExtractor
 from opengever.base.transport import FIELDDATA_KEY
 from opengever.dossier.behaviors.dossier import IDossier
+from opengever.dossier.deactivate import DossierDeactivator
+from opengever.dossier.interfaces import IDossierResolver
 from opengever.maintenance.debughelpers import setup_app
 from opengever.maintenance.debughelpers import setup_option_parser
 from opengever.maintenance.debughelpers import setup_plone
@@ -27,6 +30,7 @@ from persistent.mapping import PersistentMapping
 from plone import api
 from plone.subrequest import subrequest
 from zope.annotation import IAnnotations
+from zope.component import getAdapter
 from zope.component import provideUtility
 from zope.globalrequest import getRequest
 from zope.interface import alsoProvides
@@ -40,7 +44,7 @@ logging.root.setLevel(logging.INFO)
 handler = logging.StreamHandler(stream=sys.stdout)
 logging.root.addHandler(handler)
 
-PATH_MAPPING_KEY = 'MEETING_MIGRATION_PATH_MAPPING'
+MEETING_MIGRATION_KEY = 'MEETING_MIGRATION_DATA'
 
 
 class PreconditionsError(Exception):
@@ -65,6 +69,7 @@ class MeetingsContentMigrator(object):
         self._task_queue = self.setup_task_queue()
         self.replace_meeting_dossier_with_normal_dossier()
         self.migrate_agendaitems_to_subdossiers()
+        self.set_meeting_dossier_state()
         self.disable_meeting_feature()
 
         if not self.dryrun:
@@ -103,7 +108,31 @@ class MeetingsContentMigrator(object):
 
             self.log_and_write_table(active_proposals_table, "Active Proposals", "active_proposals")
 
-        if active_meetings.count() or active_proposals.count():
+        # Meeting dossiers should be in state active, resolved or inactive
+        meeting_dossiers = self.catalog.unrestrictedSearchResults(portal_type="opengever.meeting.meetingdossier")
+        allowed_states = ['dossier-state-resolved', 'dossier-state-inactive', 'dossier-state-active']
+        dossiers_in_bad_state = []
+        for brain in meeting_dossiers:
+            if brain.review_state not in allowed_states:
+                dossiers_in_bad_state.append(brain)
+
+        if dossiers_in_bad_state:
+            dossiers_in_bad_state_table = TextTable()
+            dossiers_in_bad_state_table.add_row(
+                (u"Path", u"Title", u"State"))
+
+            for brain in dossiers_in_bad_state:
+                dossiers_in_bad_state_table.add_row((
+                    brain.getPath(),
+                    brain.title.replace(",", ""),
+                    brain.review_state))
+
+            self.log_and_write_table(
+                dossiers_in_bad_state_table,
+                "Dossiers in bad review states",
+                "dossiers_in_bad_state")
+
+        if active_meetings.count() or active_proposals.count() or dossiers_in_bad_state:
             raise PreconditionsError("Preconditions not satisfied")
 
     def replace_meeting_dossier_with_normal_dossier(self):
@@ -134,6 +163,10 @@ class MeetingsContentMigrator(object):
             # update favorites
             query = Favorite.query.by_object(meeting_dossier)
             query.update({'oguid': Oguid.for_object(dossier)})
+
+            # Save state of the meeting dossier on the new dossier
+            migration_annotations = self.get_migration_annotations(dossier)
+            migration_annotations['state'] = api.content.get_state(meeting_dossier)
 
             # We will delete the dossiers afterwards, as it otherwise seems
             # to modify meeting_dossiers during iteration.
@@ -170,30 +203,50 @@ class MeetingsContentMigrator(object):
                 document = agendaitem.resolve_document()
                 self.move_and_add_to_mapping(document, dossier, meeting_dossier)
 
+    def set_meeting_dossier_state(self):
+        for meeting in Meeting.query:
+            meeting_dossier = meeting.get_dossier()
+            migration_annotations = self.get_migration_annotations(meeting_dossier)
+            state = migration_annotations['state']
+            if state == 'dossier-state-active':
+                continue
+            elif state == 'dossier-state-resolved':
+                resolver = getAdapter(meeting_dossier, IDossierResolver, name="lenient")
+                resolver.raise_on_failed_preconditions()
+                resolver.resolve()
+            elif state == 'dossier-state-inactive':
+                DossierDeactivator(meeting_dossier).deactivate()
+            else:
+                logger.info("Could not set state {} for {}".format(
+                                state, meeting_dossier.absolute_url_path()))
+            api.content.transition(meeting_dossier, to_state=migration_annotations['state'])
+
     def disable_meeting_feature(self):
         api.portal.set_registry_record(
             'is_feature_enabled', False, interface=IMeetingSettings)
 
-    def get_path_mapping(self, obj):
+    def get_migration_annotations(self, obj):
         annotations = IAnnotations(obj)
-        if PATH_MAPPING_KEY not in annotations:
-            annotations[PATH_MAPPING_KEY] = PersistentMapping()
-        return annotations[PATH_MAPPING_KEY]
+        if MEETING_MIGRATION_KEY not in annotations:
+            annotations[MEETING_MIGRATION_KEY] = PersistentMapping()
+            annotations[MEETING_MIGRATION_KEY]['copied'] = PersistentMapping()
+            annotations[MEETING_MIGRATION_KEY]['moved'] = PersistentMapping()
+        return annotations[MEETING_MIGRATION_KEY]
 
     def copy_and_add_to_mapping(self, obj, container, meeting_dossier):
         former_path = obj.absolute_url_path()
         copied = api.content.copy(source=obj, target=container)
         new_path = copied.absolute_url_path()
-        mapping = self.get_path_mapping(meeting_dossier)
-        mapping[new_path] = former_path
+        migration_annotations = self.get_migration_annotations(meeting_dossier)
+        migration_annotations['copied'][new_path] = former_path
         return copied
 
     def move_and_add_to_mapping(self, obj, container, meeting_dossier):
         former_path = obj.absolute_url_path()
         moved = api.content.move(obj, container)
         new_path = moved.absolute_url_path()
-        mapping = self.get_path_mapping(meeting_dossier)
-        mapping[new_path] = former_path
+        migration_annotations = self.get_migration_annotations(meeting_dossier)
+        migration_annotations['moved'][new_path] = former_path
         return moved
 
     def log_and_write_table(self, table, title, filename):
@@ -234,7 +287,9 @@ class MeetingsContentMigrator(object):
 
 def main():
     app = setup_app()
-    setup_plone(app)
+    portal = setup_plone(app)
+    portal.setupCurrentSkin()
+    alsoProvides(getRequest(), IOpengeverBaseLayer)
 
     parser = setup_option_parser()
     parser.add_option("-n", dest="dryrun", action="store_true", default=False)
