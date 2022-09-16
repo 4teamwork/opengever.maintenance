2703bf3aa95e8227d3d1f664a1b0795ba0477ed6
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-09-16 15:30:27 +0200

    Add script to export and close all dossiers inside a repofolder.

---
+++ opengever/maintenance/scripts/export_and_close_dossiers.py
@@ -0,0 +1,213 @@
+"""
+This script is used to export all the dossiers in a given repofolder and close
+them.
+"""
+from ftw.upgrade.progresslogger import ProgressLogger
+from opengever.document.behaviors import IBaseDocument
+from opengever.dossier.behaviors.dossier import IDossierMarker
+from opengever.dossier.interfaces import IDossierResolver
+from opengever.maintenance.debughelpers import setup_app
+from opengever.maintenance.debughelpers import setup_option_parser
+from opengever.maintenance.debughelpers import setup_plone
+from opengever.maintenance.utils import LogFilePathFinder
+from opengever.maintenance.utils import TextTable
+from plone import api
+from zope.component import getAdapter
+import logging
+import os
+import sys
+import time
+import transaction
+
+logger = logging.getLogger('opengever.maintenance')
+logging.root.setLevel(logging.INFO)
+handler = logging.StreamHandler(stream=sys.stdout)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logging.root.addHandler(handler)
+
+TIMESTAMP = time.strftime('%d%m%Y-%H%M%S')
+
+
+class PreconditionsError(Exception):
+    """Raised when preconditions for the migration are not satisfied"""
+
+
+class DisallowedReviewState(Exception):
+    """Raised when trying to export a dossier not in an allowed final state"""
+
+
+class DossierExporter(object):
+
+    allowed_final_states = ['dossier-state-resolved', 'dossier-state-inactive']
+    states_to_resolve = ['dossier-state-active']
+
+    def __init__(self, context, output_directory, check_only=False, dryrun=False):
+        self.check_only = check_only
+        self.dryrun = dryrun
+        self.output_directory = output_directory
+        self.context = context
+        self.catalog = api.portal.get_tool("portal_catalog")
+
+    @property
+    def dossiers(self):
+        return self.catalog.unrestrictedSearchResults(
+            path=self.context.absolute_url_path(),
+            object_provides=IDossierMarker.__identifier__,
+            is_subdossier=False)
+
+    def __call__(self):
+        self.check_preconditions()
+        if self.check_only:
+            return
+
+        if self.dryrun:
+            logger.info('Performing dryrun!\n')
+            transaction.doom()
+
+        self.resolve_dossiers()
+
+        # create output directory
+        os.mkdir(self.output_directory)
+        self.export_dossiers()
+
+        if not self.dryrun:
+            logger.info("Committing...")
+            transaction.commit()
+
+        logger.info("Done!")
+
+    def check_preconditions(self):
+        logger.info("Checking preconditions...")
+        # All dossiers should be either inactive, resolved or active and resolvable.
+        unresolvable_dossiers = []
+        dossiers_in_bad_state = []
+        for brain in self.dossiers:
+            if brain.review_state in self.allowed_final_states:
+                continue
+            elif brain.review_state not in self.states_to_resolve:
+                dossiers_in_bad_state.append(brain)
+                continue
+
+            obj = brain.getObject()
+            resolver = getAdapter(obj, IDossierResolver, name="lenient")
+            try:
+                resolver.raise_on_failed_preconditions()
+            except:
+                unresolvable_dossiers.append(brain)
+
+        if unresolvable_dossiers or dossiers_in_bad_state:
+            self.log_and_write_table(
+                unresolvable_dossiers,
+                "Unresolvable dossiers",
+                "unresolvable_dossiers")
+
+            self.log_and_write_table(
+                dossiers_in_bad_state,
+                "Dossiers in bad review states",
+                "dossiers_in_bad_state")
+
+            raise PreconditionsError("Preconditions not satisfied")
+
+        logger.info("Preconditions satisfied.")
+
+    def resolve_dossiers(self):
+        message = "Closing dossiers..."
+        for brain in ProgressLogger(message, self.dossiers, logger):
+            if brain.review_state not in self.states_to_resolve:
+                continue
+
+            obj = brain.getObject()
+            resolver = getAdapter(obj, IDossierResolver, name="lenient")
+            resolver.raise_on_failed_preconditions()
+            resolver.resolve()
+            obj.reindexObject()
+
+        logger.info("Dossiers resolved.")
+
+    def export_dossiers(self):
+        message = "Exporting dossiers."
+        for brain in ProgressLogger(message, self.dossiers, logger):
+            if brain.review_state not in self.allowed_final_states:
+                raise DisallowedReviewState("Dossier in disallowed review state")
+            self._export_dossier(brain.getObject())
+
+    def _get_output_path(self, basedir, name, ext='', i=0):
+        if i > 0:
+            output_path = os.path.join(basedir, u"{}_{}{}".format(name, i, ext))
+        else:
+            output_path = os.path.join(basedir, u"{}{}".format(name, ext))
+        if os.path.exists(output_path):
+            return self._get_output_path(basedir, name, ext, i=i+1)
+        return output_path
+
+    def _export_dossier(self, dossier):
+        folder_path = self._get_output_path(self.output_directory, dossier.title)
+        os.mkdir(folder_path)
+        res = self.catalog.unrestrictedSearchResults(
+            path=dossier.absolute_url_path(),
+            object_provides=IBaseDocument.__identifier__)
+        for brain in res:
+            doc = brain.getObject()
+            filename, ext = os.path.splitext(doc.get_filename())
+            file_path = self._get_output_path(folder_path, filename, ext)
+            with open(file_path, "w") as fout:
+                fout.write(doc.get_file().data)
+
+    def log_and_write_table(self, brains, title, filename):
+        table = TextTable()
+        table.add_row(
+            (u"Path", u"Title", u"State"))
+
+        for brain in brains:
+            table.add_row((
+                brain.getPath(),
+                brain.title.replace(",", ""),
+                brain.review_state))
+
+        logger.info("\n{}".format(title))
+        logger.info("\n" + table.generate_output() + "\n")
+
+        log_filename = LogFilePathFinder().get_logfile_path(
+            filename, extension="csv")
+        with open(log_filename, "w") as logfile:
+            table.write_csv(logfile)
+
+
+def main():
+    app = setup_app()
+    setup_plone(app)
+
+    parser = setup_option_parser()
+    parser.add_option("--check-preconditions-only", dest="check_only", action="store_true", default=False)
+    parser.add_option("-n", "--dry-run", action="store_true",
+                      dest="dryrun", default=False)
+    parser.add_option(
+        '-o', dest='output_directory',
+        default='var/dossier_export-{}'.format(TIMESTAMP),
+        help='Path to the export output directory')
+
+    (options, args) = parser.parse_args()
+
+    if not len(args) == 1:
+        print "Missing argument, please provide a path to a repofolder"
+        sys.exit(1)
+
+    if os.path.isdir(options.output_directory):
+        logger.info("Output directory already exists")
+        sys.exit(1)
+
+    if not options.output_directory:
+        logger.info("Invalid output directory")
+        sys.exit(1)
+
+    path = args[0]
+    context = app.unrestrictedTraverse(path)
+
+    exporter = DossierExporter(context, options.output_directory,
+                               options.check_only, options.dryrun)
+    exporter()
+
+
+if __name__ == '__main__':
+    main()
