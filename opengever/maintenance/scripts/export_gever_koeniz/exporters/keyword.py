3d43e161367c775a72f9dd6768deda69cf7415f9
Elio Schmutz <elio.schmutz@humanlayer.de>
2026-09-01 14:02:23 +0200

    Implement keyword  export

--- opengever/maintenance/scripts/export_gever_koeniz/exporters/keyword.py
+++ opengever/maintenance/scripts/export_gever_koeniz/exporters/keyword.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 from opengever.maintenance.scripts.export_gever_koeniz.exporters.base import BaseExporter
+from plone import api
+import hashlib
+
+
+def generate_keyword_uid(title):
+    """Deterministic UID for a keyword, derived from its title."""
+    return unicode(hashlib.md5(title.encode('utf-8')).hexdigest())
 
 
 class KeywordExporter(BaseExporter):
@@ -12,3 +19,14 @@ class KeywordExporter(BaseExporter):
         u'Schlagwort UID',
         u'Schlagwort Bezeichnung',
     ]
+
+    def get_items(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        keywords = [kw for kw in catalog.uniqueValuesFor('Subject') if kw]
+        return sorted(keywords)
+
+    def row_for_item(self, item):
+        return [
+            generate_keyword_uid(item),
+            item,
+        ]
